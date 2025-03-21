625954a4f35222fd84318f247b35ae2075d5ebff
Jackson Willis <jack@attac.us>
2025-03-21 16:22:50 -0500

    Create wifi_credentials.h

---
+++ tha_klok/tha_klok.ino
@@ -0,0 +1,15 @@
+#include <WiFi.h>
+
+#include "wifi_credentials.h"
+const char* ssid = WIFI_CREDENTIALS_SSID;
+const char* password = WIFI_CREDENTIALS_PASSWORD;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ tha_klok/wifi_credentials.h
@@ -0,0 +1,2 @@
+#define WIFI_CREDENTIALS_SSID "My Awesome Wi-Fi Network"
+#define WIFI_CREDENTIALS_PASSWORD "hunter2"
