00b4d74d5fc7719da91c3caaea14173d0413d4de
Jackson Willis <jack@attac.us>
2025-03-21 23:07:39 -0500

    Remove scratch script

--- tha_klok/strip_wifi_credentials.py
+++
@@ -1,6 +0,0 @@
-def commit_callback(commit):
-    if commit.original_id.startswith(b'9a79d76'):
-        commit.file_changes = [
-            fc for fc in commit.file_changes
-            if fc.path != b'wifi_credentials.h'
-        ]
